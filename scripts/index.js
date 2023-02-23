930d16469932afd25dfbe581f266d4a1603d2917
Scott <56418482+WSB98@users.noreply.github.com>
2023-02-23 17:52:02 -0500

    initializing repo. new codepen of buttons and animations

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+
+
+document.getElementById('buttonSample').addEventListener('click', function (e) {
+    e.preventDefault();
+})
+
+document.getElementById("myBtn").addEventListener('click', async function () {
+    var btn = document.getElementById("myBtn");
+    shrinkButton(btn)
+    go();
+    setTimeout(reloadPage,1500)
+  });
+  
+  async function shrinkButton(btn){
+    btn.hidden = 'true';
+  }
+  
+  async function go() {
+     var holder = document.getElementById("myHolder")
+     holder.innerHTML += `<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+      <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="#none"/>
+      <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+    </svg>`
+   }
+  
+  async function reloadPage(){
+    document.getElementById("myHolder").innerHTML = `<button id="myBtn" class="myButton">Login
+    <span class="hiddenPanel">Get Your Game On</span>
+    </button>`
+    
+    document.getElementById("myBtn").addEventListener('click', async function () {
+    var btn = document.getElementById("myBtn");
+    shrinkButton(btn)
+    go();
+    setTimeout(reloadPage,1500)
+  });
+    
+   
+  }
